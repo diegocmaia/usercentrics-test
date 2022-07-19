885e97b9b03627c6c26ef6f3328e94a8069a2c1d
Diego Maia <dmaia@Diegos-MacBook-Pro-2.local>
2022-07-19 15:07:06 +0200

    init setup

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <Script
+                    strategy="beforeInteractive"
+                    src="https://app.usercentrics.eu/browser-ui/latest/loader.js"
+                    data-settings-id="FTCT55YTW"
+                     />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
